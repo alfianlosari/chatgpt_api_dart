ce43a7e07db034d432dce5d1fca35b1074a1c1a2
Alfian Losari <alfianlosari@gmail.com>
2023-03-21 16:28:54 +0700

    ix Stream issue in Safari browser,

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.1.1
+
+* Fix Stream issue in Safari browser, for now it will wait for chunks to be completed. This will be fixed when `dart:http` fix the streamed response issue in Web
+
 ## 1.1.0
 
 * Fix HTTP Stream on Web
--- lib/src/stream_client_web.dart
+++ lib/src/stream_client_web.dart
@@ -1,12 +1,20 @@
 import 'package:http/http.dart' as http;
 import 'stream_client.dart';
 import 'package:fetch_client/fetch_client.dart';
+import 'package:web_browser_detect/web_browser_detect.dart';
 
 class StreamClientWeb extends StreamClient {
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
-    final client = FetchClient(mode: RequestMode.cors, streamRequests: true);
-    return client.send(request);
+    final browser = Browser().browser;
+    final isSafariBrowser =
+        browser.indexOf("Safari") > -1 && browser.indexOf("Chrome") <= -1;
+    if (isSafariBrowser) {
+      return http.Client().send(request);
+    } else {
+      final client = FetchClient(mode: RequestMode.cors, streamRequests: true);
+      return client.send(request);
+    }
   }
 }
 
